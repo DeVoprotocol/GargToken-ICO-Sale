118f63c03fc9d9ed4bf6878861cdb4b76ef281dc
DeVoprotocol <85256670+DeVoprotocol@users.noreply.github.com>
2022-10-26 21:37:58 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = (props) => {
 	return (
 		<nav className="navbar navbar-dark bg-dark shadow mb-5">
-			<p className="navbar-brand my-auto">GargToken ICO Sale</p>
+			<p className="navbar-brand my-auto">DeVo Sale</p>
 			<ul className="navbar-nav">
 				<li className="nav-item text-white">{props.account}</li>
 			</ul>
